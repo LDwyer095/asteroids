bc75fa6801bf974331d3ddf67277947f8c30f00e
LDwyer095 <liam.innis.dwyer@gmail.com>
2025-02-28 03:55:12 -0500

    Commiting all updates up to CH3 L2

---
+++ main.py
@@ -0,0 +1,52 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+import asteroidfield
+from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
+
+def main():
+    pygame.init()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids ,updatable, drawable)
+    AsteroidField.containers = updatable
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+    asteroid_field = AsteroidField()
+
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        for x in updatable:
+            x.update(dt)
+        
+        screen.fill("black")
+
+        for x in drawable:
+            x.draw(screen)
+
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
+
+
+
+if __name__ == "__main__":
+    main()
